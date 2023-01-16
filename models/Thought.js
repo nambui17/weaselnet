b249c0d71ff092d5a1bc75b3b52c679782d69abe
Nam Bui <nam.bui17@icloud.com>
2023-01-15 20:10:35 -0600

    Thoughts, reactions, and user models

---
+++ models/Thought.js
@@ -0,0 +1,34 @@
+import { Schema, model } from 'mongoose';
+import { reactionSchema } from '../models';
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [{
+            type: reactionSchema
+        }]
+    }
+)
+
+thoughtSchema
+    .virtual('reactionCount')
+    .get(function() {
+        return this.reactions.length;
+    });
+
+const Thought = model('thought', thoughtSchema);
+
+export default Thought;
