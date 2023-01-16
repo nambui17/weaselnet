ffbb538119850210c019e6a6bf781dfc8550e623
Nam Bui <nam.bui17@icloud.com>
2023-01-16 13:36:40 -0600

    add seed files

---
+++ utils/userData.js
@@ -0,0 +1,24 @@
+const userSeed = [
+    {
+        username: "leastWeasel1",
+        email: "lW1234@gmail.com"
+    },
+    {
+        username: "blackfoot2",
+        email: "blackpod5@gmail.com"
+    },
+    {
+        username: "stoat3",
+        email: "stoatgoat@gmail.com"
+    },
+    {
+        username: "longtail4",
+        email: "taaaaaaaiiiilll@gmail.com"
+    },
+    {
+        username: "minkmink5",
+        email: "minkmink6@yahoo.com"
+    }
+]
+
+module.exports = userSeed;
